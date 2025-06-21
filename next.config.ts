a72b7849cd6f5c2fad2ffb25afb9848b3415a00c
erich <erichfaizal@gmail.com>
2025-06-21 12:47:04 +0700

    Final next.config with eslint disabled and image domain allowed

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+
+  reactStrictMode: true,
+  swcMinify: true,
+  images: {
+    domains: ['techtest.youapp.ai'],
+  },
 };
 
 export default nextConfig;
